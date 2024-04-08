874f6cf301aaa308a30ed6692d188b0d9b0f8dca
Sergei Zinchenko <sergey.zinchenko.rnd@gmail.com>
2024-04-08 18:19:11 +0300

    update llama port

--- llama/portfile.cmake
+++ llama/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
         HEAD_REF master
         SHA512 7708d1c1af7af95e5de9865fe82b4f325389fe78891e5160b2579baaabb3757fcd72378927d98b81b7cdd0f34c296588985dd45bb322570f39f0d3a13d006b71
         PATCHES
-          fix-packaging.patch
+        fix-packaging.patch
 )
 
 vcpkg_find_acquire_program(PKGCONFIG)
@@ -20,10 +20,13 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         openblas LLAMA_BLAS
 )
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    list(APPEND ADDITIONAL_FLAGS "-DLLAMA_STATIC=ON")
-endif()
+if (LLAMA_BLAS)
+    list(APPEND ADDITIONAL_FLAGS "-DLLAMA_BLAS_VENDOR=OpenBLAS")
+endif ()
 
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    list(APPEND ADDITIONAL_FLAGS "-DLLAMA_STATIC=ON")
+endif ()
 
 vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
@@ -33,7 +36,6 @@ vcpkg_cmake_configure(
         -DLLAMA_AVX2=ON
         -DLLAMA_FMA=ON
         -DLLAMA_NATIVE=OFF
-        -DLLAMA_BLAS_VENDOR=OpenBLAS
         -DBUILD_SHARED_LIBS=ON
         -DLLAMA_BUILD_TESTS=OFF
         -DLLAMA_BUILD_EXAMPLES=OFF
@@ -42,9 +44,9 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/Llama)
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
+endif ()
 
 vcpkg_copy_pdbs()
 
